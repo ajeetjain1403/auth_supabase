8d4e94c57d2f7cd5c7ef6bf5b235ab4a7b75153b
Ajeet Jain <ajeetjain492@gmail.com>
2023-12-19 18:09:31 +0530

    Initial commit

---
+++ lib/signup_screen.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+              child: Container(
+                width: 80,
+                height: 80,
+                color: Colors.amber, // Set your desired color
+              ),
+            ),
+          ),
+          Positioned(
+              top: 10,
+              left: 20,
+              right: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Create Account',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    'Please Sign Up to continue.',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      child: NameTextField()),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      child: EmailTextField()),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      child: PasswordTextField()),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          // Handle button press
+                          print('Button Pressed');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                20.0), // Adjust the value for the circular effect
+                          ),
+                          backgroundColor: Colors.amber,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0,
+                              vertical: 10.0), // Set your desired color
+                        ),
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            )
+                          ],
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 220,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Have account?'),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //       builder: (context) => const SignUpScreen()),
+                          // );
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.amber, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class NameTextField extends StatefulWidget {
+  @override
+  _NameTextFieldState createState() => _NameTextFieldState();
+}
+
+class _NameTextFieldState extends State<NameTextField> {
+  final TextEditingController _nameController = TextEditingController();
+  bool isFocused = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _nameController,
+      decoration: const InputDecoration(
+        labelText: 'Name ',
+        labelStyle: TextStyle(color: Colors.grey),
+        hintText: 'Enter your Full Name',
+        prefixIcon: Icon(Icons.person),
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+      ),
+      onTap: () {
+        // setState(() {
+        //   isFocused = true;
+        // });
+      },
+      onChanged: (value) {
+        // Add any necessary validation or handling logic
+      },
+    );
+  }
+}
+
+class EmailTextField extends StatefulWidget {
+  @override
+  _EmailTextFieldState createState() => _EmailTextFieldState();
+}
+
+class _EmailTextFieldState extends State<EmailTextField> {
+  final TextEditingController _emailController = TextEditingController();
+  bool isFocused = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _emailController,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        labelStyle: TextStyle(color: Colors.grey),
+        hintText: 'Enter your email',
+        prefixIcon: Icon(Icons.email),
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+      ),
+      onTap: () {
+        // setState(() {
+        //   isFocused = true;
+        // });
+      },
+      onChanged: (value) {
+        // Add any necessary validation or handling logic
+      },
+    );
+  }
+}
+
+class PasswordTextField extends StatefulWidget {
+  @override
+  _PasswordTextFieldState createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  final TextEditingController _passwordController = TextEditingController();
+  bool isFocused = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _passwordController,
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        labelStyle: const TextStyle(color: Colors.grey),
+        hintText: 'Enter your Password',
+        prefixIcon: const Icon(Icons.password_rounded),
+        // suffix: GestureDetector(
+        //   onTap: () {
+        //     // Implement your "Forgot Password?" action here
+        //     print('Forgot Password?');
+        //   },
+        //   child: const Text(
+        //     'FORGOT ?',
+        //     style: TextStyle(
+        //       color: Colors.amber,
+        //     ),
+        //   ),
+        // ),
+        border: OutlineInputBorder(),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+      ),
+      onTap: () {
+        // setState(() {
+        //   isFocused = true;
+        // });
+      },
+      onChanged: (value) {
+        // Add any necessary validation or handling logic
+      },
+    );
+  }
+}
